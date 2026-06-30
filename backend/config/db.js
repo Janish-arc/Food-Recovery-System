fbfac0f19ad068f03de2e1d67fe63a618caa012b
Janish-arc <infantjanish27@gmail.com>
2026-06-30 21:21:54 +0530

    Fix CORS

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,23 @@
+// import mongoose from "mongoose";
+
+// export const connectiondb = () => {
+//     mongoose
+//     .connect (process.env.MONGO_URL)
+//     .then((data) => {
+//         console.log("Mongodb is connected: ", data.connection.host)
+//     })
+// }
+
+
 import mongoose from "mongoose";
 
-export const connectiondb = () => {
-    mongoose
-    .connect (process.env.MONGO_URL)
-    .then((data) => {
-        console.log("Mongodb is connected: ", data.connection.host)
-    })
-}
+export const connectiondb = async () => {
+    try {
+        const data = await mongoose.connect(process.env.MONGO_URL);
 
-
+        console.log("MongoDB Connected:", data.connection.host);
+    } catch (err) {
+        console.error("MongoDB Error:");
+        console.error(err);
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ cloudinary.config({
     });
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://food-recovery-system.vercel.app"],
     credentials: true,
   })
 );

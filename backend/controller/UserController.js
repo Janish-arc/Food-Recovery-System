836bd2a72f214be1c9dc91937b920e8d41594f6e
Janish-arc <infantjanish27@gmail.com>
2026-06-30 22:05:17 +0530

    Fix authentication cookie configuration

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -120,7 +120,7 @@ export const LoginUser = async(req, res, next) => {
 //LogOut User
 export const LogoutUser = async(req, res, next) => {
     res.cookie("token", null, {
-        expires: new Date(Date.now()), httpOnly: true
+        expires: new Date(Date.now()), httpOnly: true, secure: true, sameSite: "None"
     })
     return res.status(200).json({success: true, message: "Logged out successfully"})
 }
